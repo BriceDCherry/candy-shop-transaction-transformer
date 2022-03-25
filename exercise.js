256cbecc5c25b458224510725e6938c2a4fe69c1
bcherry4 <bcherry4@ford.com>
2022-03-25 13:23:04 -0600

    Add function salesday

--- exercise.js
+++ exercise.js
@@ -1,5 +1,19 @@
+function candySalesToObject(sales){
+    return {
+        item: sales[0],
+        price: +sales[1],
+        quantity: sales[2]
+    }
+}
+
+function salesDayToObject(date){
+    return {
+        date: Object.keys(date),
+    }
+}
+
 module.exports = {
     candySalesToObject,
     salesDayToObject,
-    allSalesToArray
+  //  allSalesToArray
 };
